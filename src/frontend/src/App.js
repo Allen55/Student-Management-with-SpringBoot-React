d33c1ecc23c552d87baa1563e9faa1858a976600
Allen Harper <aharper1609@gmail.com>
2022-03-24 23:08:53 -0500

    API calls and Table set up

--- src/frontend/src/App.js
+++ src/frontend/src/App.js
@@ -1,23 +1,15 @@
-import logo from './logo.svg';
 import './App.css';
+import { Button } from 'antd';
+import { getAllStudents } from "./client";
 
 function App() {
+
+    getAllStudents()
+        .then(data => console.log())
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <Button type="primary">Button</Button>
     </div>
   );
 }
